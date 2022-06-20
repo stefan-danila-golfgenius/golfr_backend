e38018007070ba2cedffa8e820c0425c10c86692
stefan-danila-golfgenius <106440100+stefan-danila-golfgenius@users.noreply.github.com>
2022-06-20 17:11:55 +0300

    Add user view endpoint

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,26 @@ module Api
   # Controller that handles authorization and user data fetching
   class UsersController < ApplicationController
     include Devise::Controllers::Helpers
+    before_action :logged_in!, only: :show
+
+
+    def show
+      user = User.find(params[:id])
+
+      if user
+        render json: {
+          id: user.id,
+          name: user.name,
+          scores: user.scores
+        }
+      else
+        render json: {
+          errors: "The user was not found"
+        }, status: :bad_request
+        return
+      end
+    end
+
 
     def login
       user = User.find_by('lower(email) = ?', params[:email])
